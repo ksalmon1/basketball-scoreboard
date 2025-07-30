b5a7fcbfd87abef4419bda869d6abee812275abf
ksalmon1 <ksalmon001@gmail.com>
2025-07-29 23:57:32 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function add1HomeScore() {
+    let homeScoreEl = document.getElementById("home")
+    let homeScore = parseInt(homeScoreEl.textContent)
+    homeScoreEl.textContent = homeScore + 1
+}
+
+function add2HomeScore() {
+    let homeScoreEl = document.getElementById("home")
+    let homeScore = parseInt(homeScoreEl.textContent)
+    homeScoreEl.textContent = homeScore + 2
+}
+
+function add3HomeScore() {
+    let homeScoreEl = document.getElementById("home")
+    let homeScore = parseInt(homeScoreEl.textContent)
+    homeScoreEl.textContent = homeScore + 3
+}
+
+function add1AwayScore() {
+    let awayScoreEl = document.getElementById("away")
+    let awayScore = parseInt(awayScoreEl.textContent)
+    awayScoreEl.textContent = awayScore + 1
+}
+
+function add2AwayScore() {
+    let awayScoreEl = document.getElementById("away")
+    let awayScore = parseInt(awayScoreEl.textContent)
+    awayScoreEl.textContent = awayScore + 2
+}
+
+function add3AwayScore() {
+    let awayScoreEl = document.getElementById("away")
+    let awayScore = parseInt(awayScoreEl.textContent)
+    awayScoreEl.textContent = awayScore + 3
+}
+
